4f1ce8a44535ca2452b280b9287e82447749ba23
animeshryu <animeshryu@gmail.com>
2024-04-17 15:42:24 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,164 @@
+import React from 'react'
+import { motion } from "framer-motion"
+
+const App = () => {
+  return (
+    <motion.div className='bg-black flex justify-center items-center max-h-screen'>
+<motion.svg width="620" height="800" viewBox="0 0 620 472" fill="none" xmlns="http://www.w3.org/2000/svg">
+
+<motion.ellipse         initial={{ cy: 455.5 - 5 }} // Initial cy-position slightly above the center
+        animate={{
+          cy: [455.5 - 5, 455.5 + 5, 455.5 - 5], // Values for back and forth motion
+        }}
+        transition={{ duration: 2, ease: "easeInOut", repeat: Infinity, repeatType: "reverse", delay: 0.75 }} cx="310" cy="455.5" rx="110" ry="16.5" fill="url(#paint0_linear_43_25)"/>
+
+<motion.ellipse initial={{ cy: 431.5 - 5 }} // Initial cy-position slightly above the center
+        animate={{
+          cy: [431.5 - 5, 431.5 + 5, 431.5 - 5], // Values for back and forth motion
+        }}
+        transition={{ duration: 2, ease: "easeInOut", repeat: Infinity, repeatType: "reverse", delay: 0.7 }} cx="313.5" cy="431.5" rx="163.5" ry="24.5" fill="url(#paint1_linear_43_25)"/>
+
+<motion.ellipse initial={{ cy: 400.5 - 5 }} // Initial cy-position slightly above the center
+        animate={{
+          cy: [400.5 - 5, 400.5 + 5, 400.5 - 5], // Values for back and forth motion
+        }}
+        transition={{ duration: 2, ease: "easeInOut", repeat: Infinity, repeatType: "reverse", delay: 0.65 }} cx="310.5" cy="400.5" rx="210.5" ry="31.5" fill="url(#paint2_linear_43_25)"/>
+
+<motion.ellipse initial={{ cy: 369.5 - 5 }} // Initial cy-position slightly above the center
+        animate={{
+          cy: [369.5 - 5, 369.5 + 5, 369.5 - 5], // Values for back and forth motion
+        }}
+        transition={{ duration: 2, ease: "easeInOut", repeat: Infinity, repeatType: "reverse", delay: 0.6 }} cx="310" cy="369.5" rx="243" ry="36.5" fill="url(#paint3_linear_43_25)"/>
+
+<motion.ellipse initial={{ cy: 339 - 5 }} // Initial cy-position slightly above the center
+        animate={{
+          cy: [339 - 5, 339 + 5, 339 - 5], // Values for back and forth motion
+        }}
+        transition={{ duration: 2, ease: "easeInOut", repeat: Infinity, repeatType: "reverse", delay: 0.55 }} cx="313" cy="339" rx="268" ry="40" fill="url(#paint4_linear_43_25)"/>
+
+<motion.ellipse initial={{ cy: 301.5 - 5 }} // Initial cy-position slightly above the center
+        animate={{
+          cy: [301.5 - 5, 301.5 + 5, 301.5 - 5], // Values for back and forth motion
+        }}
+        transition={{ duration: 2, ease: "easeInOut", repeat: Infinity, repeatType: "reverse", delay: 0.5 }} cx="310" cy="301.5" rx="290" ry="43.5" fill="url(#paint5_linear_43_25)"/>
+
+<motion.ellipse initial={{ cy: 263.5 - 5 }} // Initial cy-position slightly above the center
+        animate={{
+          cy: [263.5 - 5, 263.5 + 5, 263.5 - 5], // Values for back and forth motion
+        }}
+        transition={{ duration: 2, ease: "easeInOut", repeat: Infinity, repeatType: "reverse", delay: 0.45 }} cx="313.5" cy="263.5" rx="303.5" ry="45.5" fill="url(#paint6_linear_43_25)"/>
+
+<motion.ellipse initial={{ cy: 224.5 - 5 }} // Initial cy-position slightly above the center
+        animate={{
+          cy: [224.5 - 5, 224.5 + 5, 224.5 - 5], // Values for back and forth motion
+        }}
+        transition={{ duration: 2, ease: "easeInOut", repeat: Infinity, repeatType: "reverse", delay: 0.4 }} cx="310" cy="224.5" rx="310" ry="46.5" fill="url(#paint7_linear_43_25)"/>
+<motion.ellipse         initial={{ cy: 188.5 - 5 }} // Initial cy-position slightly above the center
+        animate={{
+          cy: [188.5 - 5, 188.5 + 5, 188.5 - 5], // Values for back and forth motion
+        }}
+        transition={{ duration: 2, ease: "easeInOut", repeat: Infinity, repeatType: "reverse", delay: 0.35 }} cx="313.5" cy="188.5" rx="303.5" ry="45.5" fill="url(#paint8_linear_43_25)"/>
+<motion.ellipse         initial={{ cy: 150.5 - 5 }} // Initial cy-position slightly above the center
+        animate={{
+          cy: [150.5 - 5, 150.5 + 5, 150.5 - 5], // Values for back and forth motion
+        }}
+        transition={{ duration: 2, ease: "easeInOut", repeat: Infinity, repeatType: "reverse", delay: 0.3 }} cx="313" cy="150.5" rx="290" ry="43.5" fill="url(#paint9_linear_43_25)"/>
+
+<motion.ellipse initial={{ cy: 118 - 5 }} // Initial cy-position slightly above the center
+        animate={{
+          cy: [118 - 5, 118 + 5, 118 - 5], // Values for back and forth motion
+        }}
+        transition={{ duration: 2, ease: "easeInOut", repeat: Infinity, repeatType: "reverse", delay: 0.25 }} cx="313" cy="118" rx="268" ry="40" fill="url(#paint10_linear_43_25)"/>
+
+<motion.ellipse initial={{ cy: 89.5 - 5 }} // Initial cy-position slightly above the center
+        animate={{
+          cy: [89.5 - 5, 89.5 + 5, 89.5 - 5], // Values for back and forth motion
+        }}
+        transition={{ duration: 2, ease: "easeInOut", repeat: Infinity, repeatType: "reverse", delay: 0.2 }} cx="313" cy="89.5" rx="243" ry="36.5" fill="url(#paint11_linear_43_25)"/>
+
+<motion.ellipse initial={{ cy: 64.5 - 5 }} // Initial cy-position slightly above the center
+        animate={{
+          cy: [64.5 - 5, 64.5 + 5, 64.5 - 5], // Values for back and forth motion
+        }}
+        transition={{ duration: 2, ease: "easeInOut", repeat: Infinity, repeatType: "reverse", delay: 0.15 }} cx="313.5" cy="64.5" rx="210.5" ry="31.5" fill="url(#paint12_linear_43_25)"/>
+
+<motion.ellipse initial={{ cy: 39.5 - 5 }} // Initial cy-position slightly above the center
+        animate={{
+          cy: [39.5 - 5, 39.5 + 5, 39.5 - 5], // Values for back and forth motion
+        }}
+        transition={{ duration: 2, ease: "easeInOut", repeat: Infinity, repeatType: "reverse", delay: 0.1 }} cx="313.5" cy="39.5" rx="163.5" ry="24.5" fill="url(#paint13_linear_43_25)"/>
+
+<motion.ellipse initial={{ cy: 16.5 - 5 }} // Initial cy-position slightly above the center
+        animate={{
+          cy: [16.5 - 5, 16.5 + 5, 16.5 - 5], // Values for back and forth motion
+        }}
+        transition={{ duration: 2, ease: "easeInOut", repeat: Infinity, repeatType: "reverse" }} cx="313" cy="16.5" rx="110" ry="16.5" fill="url(#paint14_linear_43_25)"/>
+<defs>
+<linearGradient id="paint0_linear_43_25" x1="310" y1="439" x2="310" y2="464.5" gradientUnits="userSpaceOnUse">
+<stop/>
+<stop offset="1" stop-color="#666666"/>
+</linearGradient>
+<linearGradient id="paint1_linear_43_25" x1="313" y1="407" x2="313" y2="456" gradientUnits="userSpaceOnUse">
+<stop/>
+<stop offset="1" stop-color="#666666"/>
+</linearGradient>
+<linearGradient id="paint2_linear_43_25" x1="309.956" y1="369" x2="309.956" y2="432" gradientUnits="userSpaceOnUse">
+<stop/>
+<stop offset="1" stop-color="#666666"/>
+</linearGradient>
+<linearGradient id="paint3_linear_43_25" x1="309.439" y1="333" x2="309.439" y2="406" gradientUnits="userSpaceOnUse">
+<stop/>
+<stop offset="1" stop-color="#666666"/>
+</linearGradient>
+<linearGradient id="paint4_linear_43_25" x1="313" y1="299" x2="313" y2="379" gradientUnits="userSpaceOnUse">
+<stop/>
+<stop offset="1" stop-color="#666666"/>
+</linearGradient>
+<linearGradient id="paint5_linear_43_25" x1="310" y1="258" x2="310" y2="345" gradientUnits="userSpaceOnUse">
+<stop/>
+<stop offset="1" stop-color="#666666"/>
+</linearGradient>
+<linearGradient id="paint6_linear_43_25" x1="313.5" y1="218" x2="313.5" y2="309" gradientUnits="userSpaceOnUse">
+<stop/>
+<stop offset="1" stop-color="#666666"/>
+</linearGradient>
+<linearGradient id="paint7_linear_43_25" x1="310" y1="178" x2="310" y2="271" gradientUnits="userSpaceOnUse">
+<stop/>
+<stop offset="1" stop-color="#666666"/>
+</linearGradient>
+<linearGradient id="paint8_linear_43_25" x1="313.5" y1="143" x2="313.5" y2="234" gradientUnits="userSpaceOnUse">
+<stop/>
+<stop offset="1" stop-color="#666666"/>
+</linearGradient>
+<linearGradient id="paint9_linear_43_25" x1="313" y1="107" x2="313" y2="194" gradientUnits="userSpaceOnUse">
+<stop/>
+<stop offset="1" stop-color="#666666"/>
+</linearGradient>
+<linearGradient id="paint10_linear_43_25" x1="313" y1="78" x2="313" y2="158" gradientUnits="userSpaceOnUse">
+<stop/>
+<stop offset="1" stop-color="#666666"/>
+</linearGradient>
+<linearGradient id="paint11_linear_43_25" x1="312.439" y1="53" x2="312.439" y2="126" gradientUnits="userSpaceOnUse">
+<stop/>
+<stop offset="1" stop-color="#666666"/>
+</linearGradient>
+<linearGradient id="paint12_linear_43_25" x1="312.956" y1="33" x2="312.956" y2="96" gradientUnits="userSpaceOnUse">
+<stop/>
+<stop offset="1" stop-color="#666666"/>
+</linearGradient>
+<linearGradient id="paint13_linear_43_25" x1="313" y1="15" x2="313" y2="64" gradientUnits="userSpaceOnUse">
+<stop/>
+<stop offset="1" stop-color="#666666"/>
+</linearGradient>
+<linearGradient id="paint14_linear_43_25" x1="313" y1="-9.83477e-07" x2="313" y2="25.5" gradientUnits="userSpaceOnUse">
+<stop/>
+<stop offset="1" stop-color="#666666"/>
+</linearGradient>
+</defs>
+</motion.svg>
+
+    </motion.div>
+  )
+}
+
+export default App
